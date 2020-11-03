6c0c8d67aa243213d8966c567d2743464e651097
AndroShivam <52043419+AndroShivam@users.noreply.github.com>
2020-11-03 19:50:23 +0530

    Added Image Crop Library and Storage permissions

--- app/src/main/java/com/shivam/makemyday/SettingsFragment.kt
+++ app/src/main/java/com/shivam/makemyday/SettingsFragment.kt
@@ -1,16 +1,133 @@
 package com.shivam.makemyday
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.preference.PreferenceFragmentCompat
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.shivam.makemyday.databinding.FragmentSettingsBinding
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 
-class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+class SettingsFragment : Fragment() {
 
-        val sharedPreference = preferenceScreen.sharedPreferences
-        val preferenceScreen = preferenceScreen
+    private lateinit var binding: FragmentSettingsBinding
+    private lateinit var profileImageURI: Uri
+
+    private lateinit var storageReference: StorageReference
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var currentUserID: String
+
+    companion object {
+        private var STORAGE_PERMISSION_CODE = 123
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+
+        storageReference = FirebaseStorage.getInstance().reference
+        firebaseAuth = FirebaseAuth.getInstance()
+        currentUserID = firebaseAuth.currentUser?.uid.toString()
+
+        binding.settingsProfilePicture.setOnClickListener {
+            checkPermission()
+        }
+
+        binding.settingsSaveBtn.setOnClickListener {
+            val profileName = binding.settingsUsername.text.toString()
+            saveToFirestore(profileName)
+        }
+
+        return binding.root
+    }
+
+    private fun saveToFirestore(profileName: String) {
+        if (!TextUtils.isEmpty(profileName)) {
+
+            val imagePath = storageReference.child("Profile_Images").child("$currentUserID.jpg")
+
+            imagePath.putFile(profileImageURI).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    imagePath.downloadUrl.addOnSuccessListener {
+                        TODO()
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Something went wrong!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+    }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            openGallery()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                STORAGE_PERMISSION_CODE
+            )
+        }
+    }
+
+    private fun openGallery() {
+        CropImage.activity()
+            .setGuidelines(CropImageView.Guidelines.ON)
+            .setAspectRatio(1, 1)
+            .start(requireContext(), this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(data)
+            if (resultCode == Activity.RESULT_OK) {
+                profileImageURI = result.uri
+                binding.settingsProfilePicture.setImageURI(profileImageURI)
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Toast.makeText(requireContext(), "Error : ${result.error}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
 
 
+        when (requestCode) {
+            STORAGE_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openGallery()
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
     }
 }
